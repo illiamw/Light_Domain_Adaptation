b5844958589a0cadb6c65372b7b60384eb609972
William Ferreira <william.luis.ferreira@usp.br>
2024-11-03 15:04:03 -0300

    Release Graduation

--- pixmatch/datasets/cityscapes_Dataset.py
+++ pixmatch/datasets/cityscapes_Dataset.py
@@ -12,7 +12,7 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 IMG_MEAN = np.array((104.00698793, 116.66876762,
                      122.67891434), dtype=np.float32)
-NUM_CLASSES = 19
+NUM_CLASSES = 3
 
 # For visualization
 # label_colours = list(map(tuple, [
